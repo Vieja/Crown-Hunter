4fcea1e1d9fe7f81c57f402c0a8e7176394fa58d
Vieja <mwiechcz@gmail.com>
2020-05-06 01:11:36 +0200

    improve home screen

--- app/src/main/java/com/vieja/crownhunter/activity/HomeFragment.java
+++ app/src/main/java/com/vieja/crownhunter/activity/HomeFragment.java
@@ -16,6 +16,8 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_home, null);
 
     }
