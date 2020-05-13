ded50cc78d99771ca3dd539728a51fdd8d57ce41
Vieja <mwiechcz@gmail.com>
2020-05-13 11:51:00 +0200

    add ad strings

--- app/src/main/java/com/vieja/crownhunter/activity/MainActivity.java
+++ app/src/main/java/com/vieja/crownhunter/activity/MainActivity.java
@@ -1,12 +1,7 @@
 package com.vieja.crownhunter.activity;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,15 +15,10 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.vieja.crownhunter.MonsterDatabase;
 import com.vieja.crownhunter.R;
-import com.vieja.crownhunter.activity.EventsFragment;
-import com.vieja.crownhunter.activity.HomeFragment;
-import com.vieja.crownhunter.activity.MonstersFragment;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.preference.PreferenceManager;
 
 
 public class MainActivity extends AppCompatActivity
@@ -47,10 +37,8 @@ public class MainActivity extends AppCompatActivity
         navView.setOnNavigationItemSelectedListener(this);
         navView.setItemIconTintList(null);
 
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
-
+        MobileAds.initialize(this, getString(R.string.ad_publisher));
         adContainerView = findViewById(R.id.ad_container);
-        // Step 1 - Create an AdView and set the ad unit ID on it.
         adView = new AdView(this);
         adView.setAdUnitId(getString(R.string.ad_main));
         adContainerView.addView(adView);
@@ -74,34 +62,21 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void loadBanner() {
-        // Create an ad request. Check your logcat output for the hashed device ID
-        // to get test ads on a physical device, e.g.,
-        // "Use AdRequest.Builder.addTestDevice("ABCDE0123") to get test ads on this
-        // device."
         AdRequest adRequest =
                 new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                         .build();
-
         adSize = getAdSize();
-        // Step 4 - Set the adaptive ad size on the ad view.
         adView.setAdSize(adSize);
-
-        // Step 5 - Start loading the ad in the background.
         adView.loadAd(adRequest);
     }
 
     private AdSize getAdSize() {
-        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
         Display display = getWindowManager().getDefaultDisplay();
         DisplayMetrics outMetrics = new DisplayMetrics();
         display.getMetrics(outMetrics);
-
         float widthPixels = outMetrics.widthPixels;
         float density = outMetrics.density;
-
         int adWidth = (int) (widthPixels / density);
-
-        // Step 3 - Get adaptive ad size and return for setting on the ad view.
         return AdSize.getPortraitBannerAdSizeWithWidth(this, adWidth);
     }
 
